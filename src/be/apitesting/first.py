46321fccb60233a4925184b1829ab4579eb7ed09
Poojitha Anchetty <panchetty@healsecurity.com>
2023-03-03 12:42:58 -0600

    second commit

--- src/be/apitesting/first.py
+++ src/be/apitesting/first.py
@@ -14,11 +14,7 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.put("/users/{user_id}")
-def send_warning(user_id: int):
-    return {"someone tried to malfunction your login details, reference user id": user_id}
-
-@app.post("/items")
+@app.post("/users")
 async def getInformation(info : Request):
     req_info = await info.json()
     return {
