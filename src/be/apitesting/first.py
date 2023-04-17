9f275c2cbb6fac2b1bd2132edfef2980d34bfe51
Poojitha Anchetty <panchetty@healsecurity.com>
2023-04-17 08:46:27 -0500

    adding api code for specific selected topic in backend

--- src/be/apitesting/first.py
+++ src/be/apitesting/first.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI,Request,Query
 app = FastAPI()
 
 const articles = ["article 1 displays about security measures","article 2 displays about innovative approaches on authenticative measures"]
-const title_array = [{"title":"Details page1","description":"We describe the details of page1 and its pros and cons","bg-img-url":"dummy.png"},{},{}]
+const title_array = [{"title":"Details page1","description":"We describe the details of page1 and its pros and cons","bg-img-url":"dummy.png","tags":["page1","details"]},{},{}]
 
 @app.get("/warn_users")
 def read_root():
@@ -57,8 +57,8 @@ async def articles(info:Request):
     }
 
 ###Response:---
-# {"status":"SUCCESS","data":[{"title":"Details page1","description":"We describe the details of page1 and its pros and cons","img-url":"dummy.png"},{},{}]}
-@app.post("/details_page")
+# {"status":"SUCCESS","data":[{"title":"Details page1","description":"We describe the details of page1 and its pros and cons","img-url":"dummy.png","tags":["details","page1"]},{},{}]}
+@app.post("/details_page/{to}")
 async def details(info:Request):
     result = await info.json()
 
@@ -67,12 +67,32 @@ async def details(info:Request):
         "data":title_array
     }
 
-#this displays the cards related to tags()
-@app.post("/details_page/tags/{tag_id}")
-async def get_tag_cards(tag:Request):
-    result = await tag.json()
+# #this displays the cards related to tags()
+# @app.post("/details_page/tags/{tag_id}")
+# async def get_tag_cards(tag:Request):
+#     result = await tag.json()
 
-    return{
-        "status":"SUCCESS",
-        "data":result
-    }
+#     return{
+#         "status":"SUCCESS",
+#         "data":result
+#     }
+
+
+#this displays entire details of the topic which is selected
+#topic_name will be passed from front end as input to this api and the api will give expected results for eg:: title_array,,
+@app.post("/details_page/{topic_name}")
+async def get_tag_cards(info:Request):
+    result = await info.json()
+    #this has name, description,relevant image, relevant tags as such..
+    if(success):
+       return{
+            "status":"SUCCESS",
+            "status_code":200
+            "data":title_array
+        } 
+    else:
+        return{
+            "status":"FAILED",
+            "status_code":400
+        }
+        
